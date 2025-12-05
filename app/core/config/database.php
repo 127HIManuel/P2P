10bc4a9e922689aede44b12e5cecb32defc125d7
127HIManuel <abodunrinoluwatomiwo@gmail.com>
2025-12-05 04:29:55 +0100

    Database parameters updated for production environment.

--- app/core/config/database.php
+++ app/core/config/database.php
@@ -2,7 +2,7 @@
 define('ROOT','http://localhost/project2/public/');
 define('ASSETS', 'http://localhost/project2/public/assets/');
 define('TITLE','Pathway to Purpose');
-define('DBUSER','root');//root
-define('DBNAME','p2p_db');//p2p_db
-define('DBPASS', "");//""
-define('DBHOST','localhost');//localhost
+define('DBUSER','admin');//root
+define('DBNAME','p2p-database');//p2p_db
+define('DBPASS', "Flashdrive-1");//""
+define('DBHOST','p2p-database.cebkecaq6rrl.us-east-1.rds.amazonaws.com');//localhost
