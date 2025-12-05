3bed0ef88c4142b02f46bfd1b1569efc1d160efb
127HIManuel <abodunrinoluwatomiwo@gmail.com>
2025-12-05 11:25:26 +0100

    local host update

--- app/core/config/database.php
+++ app/core/config/database.php
@@ -1,6 +1,6 @@
 <?php
-define('ROOT','http://localhost/project2/public/');
-define('ASSETS', 'http://localhost/project2/public/assets/');
+define('ROOT','http://107.22.107.242/P2P/public/');
+define('ASSETS', 'http://107.22.107.242/P2P/public/assets/');
 define('TITLE','Pathway to Purpose');
 define('DBUSER','admin');//root
 define('DBNAME','p2p-database');//p2p_db
