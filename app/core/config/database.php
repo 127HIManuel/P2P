af090e4145e619525447e6b4b53efd2f14e5139b
127HIManuel <abodunrinoluwatomiwo@gmail.com>
2025-12-07 14:25:51 +0100

    trial6

--- app/core/config/database.php
+++ app/core/config/database.php
@@ -1,6 +1,5 @@
 <?php
 define('ROOT','http://107.22.107.242/P2P/public/');
-define('ROOT1','localhost/project2/public/');
 define('ASSETS', 'http://107.22.107.242/P2P/public/assets/');
 define('TITLE','Pathway to Purpose');
 define('DBUSER','admin');//root
